11d196d25f27ddd1d4d44c91ff78421f59a42843
Dan <djh242@cam.ac.uk>
2018-01-20 14:31:51 +0000

    added get_images

--- RedditImageFetch.py
+++ RedditImageFetch.py
@@ -1,3 +1,11 @@
 import praw
-r = praw.Reddit(client_id = 'hackcambridgebot', username = "hackcambridge2k18", password = "hackcambridge2k18")
+r = praw.Reddit(client_id = "hackcambridgebot",
+				username = "hackcambridge2k18", 
+				password = "hackcambridge2k18")
 r.login
+
+
+def get_images(r, subreddit_name):
+	subreddit = r.subreddit(subreddit_name).top('day')
+	return subreddit
+	

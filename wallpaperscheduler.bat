b9cc9c13543acc655b672f3c99204f006d91a470
coolnicegoodone <mrtimjepsen@gmail.com>
2018-01-20 19:09:26 +0000

    Updated the batch to allow two parameters

--- wallpaperscheduler.bat
+++ wallpaperscheduler.bat
@@ -1,7 +1,7 @@
 @echo off
 title this is the scheduler batch script
 
-SCHTASKS /Create /TN WallpaperUpdater /TR "%~dp0test.py %1" /sc DAILY
+SCHTASKS /Create /TN WallpaperUpdater /TR "%~dp0RedditImageFetch.py %1 %2" /sc DAILY
 
 SCHTASKS /Run /TN WallpaperUpdater
 

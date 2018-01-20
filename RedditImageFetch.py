19f0bff8cdca4ada255c87a1bc688bf34af1ca8a
Dan <djh242@cam.ac.uk>
2018-01-20 14:47:21 +0000

    It can get image urls now

--- RedditImageFetch.py
+++ RedditImageFetch.py
@@ -1,11 +1,17 @@
 import praw
-r = praw.Reddit(client_id = "hackcambridgebot",
+r = praw.Reddit(client_id = "zAXTI9GjBt2lqA",
+				user_agent = "hackcambridgebot",
+				client_secret = "oTqNPVMXXVzQM2McW_c0TV5dCTY",
 				username = "hackcambridge2k18", 
 				password = "hackcambridge2k18")
-r.login
 
-
-def get_images(r, subreddit_name):
-	subreddit = r.subreddit(subreddit_name).top('day')
-	return subreddit
+def get_image_urls(r, subreddit_name):
+	submissions = r.subreddit(subreddit_name).top('day')
+	img_urls = []
+	for sub in submissions:
+		if not sub.is_self and sub.url.endswith(".jpg") or sub.url.endswith(".png"):
+			img_urls.append(sub.url)
+	return img_urls
+	
+print(get_image_urls(r, 'all'))
 	
